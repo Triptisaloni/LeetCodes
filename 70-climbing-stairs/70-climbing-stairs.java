f088c1c984d72063b946a1ab2a216fecc7a3ce0c
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-02-26 02:05:04 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (5.73%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -2,6 +2,8 @@ class Solution {
     
     public int count(int n, int[] temp)
     {
+//         Memoization
+        
         if(n==0)
             return 1;
         else if(n<0)
@@ -23,8 +25,30 @@ class Solution {
     public int climbStairs(int n) 
     {
         
-        int[] temp = new  int[n+1];
-        return count(n,temp);
+        // int[] temp = new  int[n+1];
+        // return count(n,temp);
+        
+         if(n==0)
+            return 1;
+        else if(n<0)
+            return 0;
+        
+         int[] s = new int[n+1];
+        
+        s[0]=1;
+        
+        for(int i =1; i<=n; i++)
+        {
+            if(i==1)
+			s[i]=s[i-1];
+            
+            else 
+                s[i]= s[i-1]+s[i-2];
+            // else
+            //      s[i]= s[i-1]+s[i-2] + s[i-3];
+        }
+        
+        return s[n];
         
     }
 }
