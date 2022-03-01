c28273caf5514de80cf8ba9b6708b2f5adfd0978
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-03-01 16:33:10 +0530

    Time: 2 ms (82.30%), Space: 48.2 MB (28.98%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int [] arr = new int[n+1];
+
+        for(int i=1; i<=n; i++)
+        {
+            if(i%2==0)
+                arr[i]= arr[i/2];
+            else
+                arr[i]=arr[i-1]+1;
+        }
+        
+        return arr;
+    }
+}
