5a76ebb7a4fe0ded1c2fcf864d4dbf0bc1acf5e1
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-03-16 20:12:27 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (76.81%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -3,33 +3,42 @@ import java.util.*;
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         
-        
-        Stack<Integer> s= new Stack<>();
-        
-        int i=0;
-        int pu =0;
-        
-        while(i<popped.length)
-        {
-            
-            while(pu<pushed.length && (s.size()==0 ||  popped [i]!=s.peek()))
-            {
-                s.push(pushed[pu]);
-                pu++;
+         int mark = -1, j = 0;
+        for (int i = 0; i < pushed.length; i++){
+            pushed[++mark] = pushed[i];
+            while (mark >= 0 && j < popped.length &&pushed[mark] == popped[j]){
+                j++;
+                mark--;
             }
-            
-            if(s.isEmpty()  || s.peek()!= popped[i])
-            return false;
-      
-            s.pop();
-            i++;
-            
-            
         }
+        return (j == popped.length);
         
-        if(s.size()!=0)
-            return false;
+//         Stack<Integer> s= new Stack<>();
         
-        return true;
+//         int i=0;
+//         int pu =0;
+        
+//         while(i<popped.length)
+//         {
+            
+//             while(pu<pushed.length && (s.size()==0 ||  popped [i]!=s.peek()))
+//             {
+//                 s.push(pushed[pu]);
+//                 pu++;
+//             }
+            
+//             if(s.isEmpty()  || s.peek()!= popped[i])
+//             return false;
+      
+//             s.pop();
+//             i++;
+            
+            
+//         }
+        
+//         if(s.size()!=0)
+//             return false;
+        
+//         return true;
     }
 }
