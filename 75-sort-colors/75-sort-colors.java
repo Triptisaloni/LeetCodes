5e5d7164526d8ccadb2d5651abbdad306f7bf506
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-03-25 01:31:01 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (88.90%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,8 +1,39 @@
 class Solution {
     public void sortColors(int[] nums) {
+       
+        int z=0, o=0, t=0;
         
+        for(int i : nums)
+        {
+            if(i==0)
+                z++;
+            if(i==1)
+                o++;
+            if(i==2)
+                t++;
+        }
         
-        Arrays.sort(nums);
-        
+        int i=0;
+        while(i<nums.length)
+        {
+            while(z>0)
+            {
+                nums[i] =0;
+                z--;
+                i++;
+            }
+            while(o>0)
+            {
+                nums[i] =1;
+                o--;
+                i++;
+            }
+            while(t>0)
+            {
+                nums[i] =2;
+                t--;
+                i++;
+            }
+        }
     }
 }
