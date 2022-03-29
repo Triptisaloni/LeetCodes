0ad83b4020de4c8d0aae1bbd6977a0c4d4c61f84
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-03-29 23:45:23 +0530

    Time: 60 ms (5.78%), Space: 94 MB (6.78%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,25 +1,20 @@
+import java.util.*;
+
 class Solution {
-    public int findDuplicate(int[] nums) {
-	
-        long l=0;
-        long r=nums.length-1;
-    
-	   long mid;
-        while(l<r){
-            mid = l+(r-l)/2;
-            if(hasGreaterCount(nums,mid)){
-                r = mid;
-            }else{
-                l = mid+1;
-            }
-        }
-        return (int)l;
-    }
-    public boolean hasGreaterCount(int[] nums, long mid){
-        int count=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] <= mid) count++;
-        }
-    return count > mid;
+    public int findDuplicate(int[] nums) 
+    {
+        int n= nums.length;
+        HashSet<Integer> map = new HashSet<>();
+        
+         for(int i : nums)
+         {
+             if(map.contains(i))
+                 return i;
+             else
+                 map.add(i);
+         }
+        
+        
+        return -1;
     }
 }
