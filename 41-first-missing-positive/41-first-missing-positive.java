e2ee6629bc5e674284cd386932ef64fc023cdc73
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-04-25 00:17:08 +0530

    Time: 5 ms (44.38%), Space: 126.9 MB (62.10%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -5,11 +5,9 @@ class Solution {
         int min = 1;
 
         for (int j = 0; j < nums.length; j++) {
-            if (nums[j] <= 0) {
-                continue;
-            } else if (nums[j] == min) {
+            if (nums[j] == min) {
                 min++;
-                continue;
+                // continue;
             }
         }
         return min;
