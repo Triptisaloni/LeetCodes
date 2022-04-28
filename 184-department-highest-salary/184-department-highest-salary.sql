deffdd75d2b8e846d8a24d017d32eefee9e021c4
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-04-28 23:23:12 +0530

    Time: 784 ms (21.80%), Space: 0B (100.00%) - LeetHub

---
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -0,0 +1,5 @@
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e LEFT JOIN Department d
+ON d.id=e.departmentId
+WHERE (e.departmentId, e.salary) in 
+(SELECT e2.departmentId, MAX(e2.salary) FROM Employee e2 GROUP By e2.departmentId);
