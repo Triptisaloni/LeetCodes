24a7a2b57db2fe79b50d408a117a5b76271bb4f7
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-05-21 23:22:15 +0530

    Time: 32 ms (39.25%), Space: 44.7 MB (64.89%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    // public int help(int[] coins, int )
+    
+    public int coinChange(int[] coins, int amount) 
+    
+    {
+            int dp[]= new int[amount+1];
+//                 dp approach
+        
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0]=0;
+        
+        for(int i=1; i<=amount; i++)
+        {
+            
+            for(int j=0; j<coins.length; j++)
+            {
+                
+                if(i-coins[j]>=0)
+                {
+                    if(dp[i-coins[j]] != Integer.MAX_VALUE)
+                    {
+                        dp[i]= Math.min(dp[i] , dp[i-coins[j]]+1);
+                    }
+                }
+            }
+            
+        }
+        
+        return dp[amount]==Integer.MAX_VALUE ? -1 : dp[amount];
+        
+    }
+}
