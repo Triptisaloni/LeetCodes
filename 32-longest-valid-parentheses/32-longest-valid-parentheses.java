2fb761f5237a9d91921c55d93dde8676ed906240
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-05-24 19:00:35 +0530

    Time: 8 ms (34.29%), Space: 43.6 MB (32.81%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,58 +1,61 @@
-// class Solution {
-//     public int longestValidParentheses(String s) {
-        
-//         int n= s.length();
-        
-//         if(n<=1)
-//             return 0;
-        
-//         Stack<Integer> st = new Stack<>();
-//         // int count =0;
-//         int maxx = 0;
-//         st.push(-1);
-        
-        
-//         for(int i=0; i<n; i++)
-//         {
-//             char c = s.charAt(i);
-            
-//            if(c=='(')
-//                st.push(i);
-//             else
-//             {
-//                 st.pop();
-//                 if(s.isEmpty())
-//                     st.push(i);
-//                 else
-//                     maxx= Math.max(maxx, i-st.peek());
-//             }
-            
-//         }
-        
-//         return maxx;
-//     }
-// }
-
-
-
 class Solution {
-	public int longestValidParentheses(String s) {
-		 int max_valid = 0;
-		 Stack<Integer> stack = new Stack<Integer>();
-		 stack.push(-1);
-
-		for(int i =0 ; i< s.length() ;i++)
-		{
-			if(s.charAt(i) == '(')
-				stack.push(i);
-			else
-			{
-			   stack.pop();
-			   if(stack.empty()) stack.push(i);
-			   else
-				  max_valid = Math.max(max_valid , i-stack.peek());
-			}
-		}
-		return max_valid;
-	}
+    public int longestValidParentheses(String s) {
+        
+        int n= s.length();
+        
+        if(n<=1)
+            return 0;
+        
+        Stack<Integer> st = new Stack<>();
+        // int count =0;
+        int maxx = 0;
+        st.push(-1);
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            char c = s.charAt(i);
+            
+           if(c=='(')
+               st.push(i);
+            else
+            {
+                st.pop();
+                if(st.isEmpty())
+                    st.push(i);
+                else
+                    maxx= Math.max(maxx, i-st.peek());
+            }
+            
+        }
+        
+        return maxx;
+    }
 }
+
+
+
+// class Solution {
+// 	public int longestValidParentheses(String s) {
+        
+// 		 int max_valid = 0;
+// 		 Stack<Integer> stack = new Stack<Integer>();
+// 		 stack.push(-1);
+
+// 		for(int i =0 ; i< s.length() ;i++)
+// 		{
+// 			if(s.charAt(i) == '(')
+// 				stack.push(i);
+// 			else
+// 			{
+// 			   stack.pop();
+                
+// 			   if(stack.empty()) 
+//                    stack.push(i);
+// 			   else
+// 				  max_valid = Math.max(max_valid , i-stack.peek());
+// 			}
+// 		}
+// 		return max_valid;
+// 	}
+// }
