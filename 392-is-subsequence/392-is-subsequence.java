650089b05aea1ccb3fc2716724d3b957ac16de24
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-06-23 23:28:27 +0530

    Time: 2 ms (44.65%), Space: 41.6 MB (78.21%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,48 +1,22 @@
 class Solution {
-    public static int sub(String s, String t)
-    {
-        int m= s.length();
-        int n = t.length();
-        
-           if(m==0||n==0)
-            return 0;
-        
-        int storage[][]=new int[m+1][n+1];
-
-        
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1; j<=n; j++)
-            {
-             if(s.charAt(m-i)==t.charAt(n-j))
-                    storage[i][j]= 1+storage[i-1][j-1];
-        else
-        {
-            storage[i][j]= Math.max(storage[i-1][j], storage[i][j-1]);
-        }
-            }
-        }
-        return storage[m][n];
-    }
-    
     public boolean isSubsequence(String s, String t) 
+    
     {
-//         int res = sub(s,t);
-//         if(res==s.length())
-//             return true;
-//         else
-//             return false;
+            
+        int i=0, j=0;
+        int sn =s.length(), tn = t.length();
         
-         int i = 0 , j = 0;
-        while(i < s.length() && j < t.length()){
-            if(s.charAt(i) == t.charAt(j)){
-                i++;
-                j++;
-            }else{
-                j++;
-            }
+        while(i<sn && j<tn)
+        {
+            // if(i==sn)
+            //     return true;
+            
+            if(s.charAt(i)==t.charAt(j))
+            i++;
+            
+            j++;
         }
-        return (i == s.length());
-     
+        
+        return (i==sn);
     }
 }
