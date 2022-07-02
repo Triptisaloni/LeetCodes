b72f5e7afebfcc745485a07ba540163ea51cffff
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-07-03 01:31:44 +0530

    Time: 19 ms (7.53%), Space: 40.9 MB (49.33%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,31 @@
 class Solution {
+/*    Normal approach
     public int fib(int n) {
         
         if(n<=1)
             return n;
         return fib(n-1)+fib(n-2);
+    }*/
+    
+    public int fib(int n)
+    {
+        int [] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return help(n, dp);
+        
+    }
+    
+    public int help(int n, int[] storage)
+    {
+        if(n<=1)
+        {
+            storage[n]=n;
+            return n;
+        }
+        
+        if(storage[n]!=-1)
+            return storage[n];
+        
+        return help(n-1,storage )+help(n-2, storage);
     }
 }
