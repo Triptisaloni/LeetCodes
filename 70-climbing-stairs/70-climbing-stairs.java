852aac59f2eb823d0424e4ab4ab6157c196413ec
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-07-06 00:09:17 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (79.44%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,65 +1,11 @@
 class Solution {
-    
-    public int count(int n, int[] temp)
-    {
-//         Memoization
-        
-        if(n==0)
-            return 1;
-        else if(n<0)
-            return 0;
-        
-        if(temp[n]>0)
-            return temp[n];
-        
-        int a = count(n-1, temp);
-        int b = count(n-2, temp);
-        // int c = count(n-3, temp);
-        
-        int path = a+b;
-        
-        temp[n]=path;
-        return path;
-    }
-    
-    public int climbStairs(int n) 
-    {
-//         Memoization
-        // int[] temp = new  int[n+1];
-        // return count(n,temp);
-        
-//         Dynamic Approach
-//          if(n==0)
-//             return 1;
-//         else if(n<0)
-//             return 0;
-        
-//          int[] s = new int[n+1];
-        
-//         s[0]=1;
-        
-//         for(int i =1; i<=n; i++)
-//         {
-//             if(i==1)
-// 			s[i]=s[i-1];
-            
-//             else 
-//                 s[i]= s[i-1]+s[i-2];
-//             
-//         }
-        
-//         return s[n];
-       
-        if(n==0 || n==1) return 1;
-        int[] nums = new int[n+1];
-        nums[0] = 1;
-        nums[1] = 1;
-        for(int i=2; i<=n; i++) {
-            nums[i] = nums[i-1] + nums[i-2];
+    public int climbStairs(int n) {
+         int[] dp= new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]= dp[i-1]+dp[i-2];
         }
-        
-        return nums[n];
-  
-        
+        return dp[n];
     }
 }
