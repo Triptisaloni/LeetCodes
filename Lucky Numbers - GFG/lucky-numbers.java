8f24d7d68e7749678654859a8f7386b4c65b8ae0
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-07-10 00:40:32 +0530

    Added solution - LeetHub

---
+++ Lucky Numbers - GFG/lucky-numbers.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(reader.readLine());//testcases
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(reader.readLine());//input n
+            //printing "1" if isLucky() returns true else "0"
+            Solution s = new Solution();
+            System.out.println(s.isLucky(n)? "1" : "0");
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution
+{
+    // Complete the function
+    // n: Input n
+    // Return True if the given number is a lucky number else return False
+    
+    public static boolean help(int n, int m)
+    {
+        if(n%m==0)
+        return false;
+        if(m>n)
+        return true;
+        
+        return help(n-(n/m), m+1);
+    }
+    
+    public static boolean isLucky(int n)
+    {
+       return help(n,2);
+    }
+}
