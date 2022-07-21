6fda555467c8d6b297280754e264fa9f4fea05be
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-07-22 01:14:04 +0530

    Time: 0 ms (100.00%), Space: 42 MB (37.90%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -10,102 +10,65 @@
  */
 class Solution {
     
-    /*public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (left == right) {
-            return head;
-        }
-        ListNode leftStart = new ListNode(-1000, head);
-        ListNode leftEnd = leftStart;
-        int i = 0;
-        while (i < left-1) {
-            leftEnd = leftEnd.next;
-            i++;
-        }
-        ListNode prev = null;
-        ListNode rEnd = leftEnd.next;
-        ListNode rStart = leftEnd.next;
-        while (rStart != null && i < right) {
-            ListNode temp = rStart;
-            rStart = rStart.next;
-            temp.next = prev;
-            prev = temp;
-            i++;
-        }
-        leftEnd.next = prev;
-        rEnd.next = rStart;
-        return leftStart.next;
-    }*/
-    
-    public ListNode reverse(ListNode head)
+    public ListNode rev(ListNode head)
     {
-        ListNode curr =head;
-        ListNode prev = null;
-        ListNode next = null;
+        ListNode prev = null, next =null, curr=head;
         
         while(curr!=null)
         {
-            next = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr = next;
+            next=curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr=next;
         }
         
         return prev;
     }
     
-//     public ListNode reverse(ListNode  head)
-// {
-//     ListNode  prev = null;   
-//     ListNode  curr = head;
- 
-//     while (curr!=null) {
-//         ListNode next = curr.next;
-//         curr.next = prev;
-//         prev = curr;
-//         curr = next;
-//     }
-//     return prev;
-// }
     
     public ListNode reverseBetween(ListNode head, int left, int right) {
         
-        if(head==null || head.next==null)
+            if(head==null || head.next==null || left==right)
             return head;
-        if(left==right)
-            return head;
-         
+       
         ListNode revstart = null, revend=null, revs_prev=null, revend_next=null;
             
         int i=1;
-        ListNode curr=head;
-       
+        ListNode curr = head;
+        
         while(curr!=null && i<=right)
         {
             if(i<left)
                 revs_prev= curr;
             if(i==left)
-                revstart=curr;
+                revstart = curr;
+            
             if(i==right)
             {
-                revend= curr;
-            revend_next=curr.next;
+                revend = curr;
+                revend_next=curr.next;
             }
-            curr=curr.next;
+            
+            curr = curr.next;
             i++;
         }
         
+        
         revend.next=null;
         
-        revend = reverse(revstart);
+        revend = rev(revstart);
         
+        // revs_prev.next = revend;
         if(revs_prev!=null)
-            revs_prev.next= revend;
+        revs_prev.next= revend;
         else
             head = revend;
         
-        revstart.next=revend_next;
+        revstart.next = revend_next;
         
         return head;
+        
+
         
     }
 }
