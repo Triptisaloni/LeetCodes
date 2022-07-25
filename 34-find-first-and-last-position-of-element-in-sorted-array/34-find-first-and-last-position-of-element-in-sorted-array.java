ea5164c297322e70e9a912cb2987421d64faadfa
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-07-25 16:10:10 +0530

    Time: 0 ms (100.00%), Space: 47.5 MB (31.00%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return new int[] {-1, -1};
         
-        int low = binarySearch(nums, target, true);
-        int high = binarySearch(nums, target, false);
+        int n = nums.length;
+        int [] res = new int[2];
+        res[0]=-1;
+        res[1]=-1;
         
-        return new int[] {low, high};
-    }
-    
-    private int binarySearch(int[] nums, int target, boolean isLowBound) {
-        int left = 0;
-        int right = nums.length - 1;
-        int res = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (target < nums[mid]) {
-                right = mid - 1;
+        if(n==0)
+            return res;
+        int l=0, r=n-1;
+        int mid =0;
+        
+        while(l<=r)
+        {
+            mid = l+(r-l)/2;
+            
+            if(nums[mid]==target)
+            {
+                int first = mid;
+                while(first>=0 && nums[first]==target)
+                     first--;
+                res[0]= first+1;
+                int last = mid;
+                while(last<n && nums[last]==target)
+                     last++;
+                res[1]= last-1;
+                
+                return res;
+                
+        
+                
             }
-            else if (target > nums[mid]) {
-                left = mid + 1;
-            }
-            else {
-                res = mid;
-                if (isLowBound) {
-                    // searching for lower bound, forcing to the left
-                    right = mid - 1;
-                }
-                else {
-                    // searching for higher bound, forcing to the right
-                    left = mid + 1;
-                }
-            }
+            else if(target<nums[mid])
+                r=mid-1;
+            else
+                l=mid+1;
         }
         
+      
         return res;
     }
 }
