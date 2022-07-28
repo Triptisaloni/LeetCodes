0c4136404364cc1c81a7ccbdb6ea6edf252057f3
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-07-28 14:46:12 +0530

    Time: 6 ms (63.45%), Space: 45.1 MB (35.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,43 +1,20 @@
-import java.util.HashMap;
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        char[] a1 = s.toCharArray();
-    char[] b1 = t.toCharArray();
-    Arrays.sort(a1); Arrays.sort(b1);
-    return Arrays.equals(a1, b1);
-//         int m= s.length();
-//         int n= t.length();
+        int sl = s.length(), tl = t.length();
+        if(sl!=tl)
+            return false;
         
-//         if(m!=n)
-//             return false;
+        char[] ss = s.toCharArray();
+        char[] tt= t.toCharArray();
         
-//         HashMap< String , Integer> map =new HashMap<>();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
         
-//         for(int i=0; i<m; i++)
-//         {
-//             String p = ""+s.charAt(i);
-//             if(map.containsKey(p))
-//             {
-//                 int val = map.get(p);
-//                 map.put(p, val+1);
-//             }
-//             else
-//             map.put(p,1);
-//         }
+        for(int i=0; i<sl;i++)
+            if(ss[i]!=tt[i])
+                return false;
         
-//         for(int i=0; i<n; i++)
-//         {
-//             String temp =""+ t.charAt(i);
-//             if(!map.containsKey(temp))
-//                 return false;
-            
-//             int val= map.get(temp);
-//             if(val==0)
-//                 return false;
-//             map.put(temp, val-1);
-//         }
-        
-//         return true;
+        return true;
     }
 }
