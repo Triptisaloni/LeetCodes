25d88f194a0a5719c2cae3f027d0569e9833d6b4
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-08-24 01:57:36 +0530

    Time: 6 ms (79.16%), Space: 97.4 MB (61.07%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -8,67 +8,73 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
-    public static ListNode mid(ListNode head)
-    {
-        ListNode slow = head, fast = head;
+    
+//     public int length(ListNode head)
+//     {
+//         if(head.next==null)
+//             return 1;
         
-        
-        while(fast.next!=null && fast.next.next!=null)
-        {
-            slow= slow.next;
-            fast=fast.next.next;
-            
-        }
-        return slow;
-        
-    }
+//         return 1 + length(head.next);
+//     }
     
     public ListNode reverse(ListNode head)
     {
-        if(head==null || head.next==null)
+         if(head==null || head.next==null)
             return head;
-        ListNode curr = head, prev=null, nex = null;
+        
+        ListNode prev = null, curr = head, next = null;
         
         while(curr!=null)
-        {
-            nex= curr.next;
-            curr.next= prev;
+        {          
+            next = curr.next;
+            // last me nhi daal skte kyukii....
+            // curr= cuur.next ho aayega or hume uska next nhi ptaaa h ki null h ya nhii
+            
+            curr.next = prev;
             prev = curr;
-            curr = nex;
+            curr = next;
+
+        }
+        return prev;
+    }
+    
+    
+    public ListNode mid(ListNode head)
+    {
+        ListNode slow = head, fast = head;
+        
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
         }
         
-     return prev;
+        return slow;
     }
+    
     
     
     public boolean isPalindrome(ListNode head) {
         
-        if(head==null || head.next==null)
+
+        if(head== null || head.next==null)
             return true;
         
-//         ListNode slow = head, fast =head;
-//         while(fast!=null && fast.next!=null)
-//         {
-//             slow = slow.next;
-//             fast = fast.next;
-//         }
+        ListNode middle = mid(head);
+        ListNode revHead = reverse(middle);
         
-        ListNode mid = mid(head);
+        // ListNode start= head;
         
-        ListNode revhead = reverse(mid);
-        // return true;
-        
-        while(head!=null && revhead!=null)
+        while(head!=null && revHead!=null)
         {
-            if(head.val!=revhead.val)
+            if(head.val!=revHead.val)
                 return false;
             
-            head=head.next;
-             revhead = revhead.next;
-
+            head= head.next;
+            revHead = revHead.next;
         }
+        
         
         return true;
     }
