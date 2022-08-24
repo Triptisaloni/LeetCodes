bf052b17d21b7f830732012444aa9655766b9cd4
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-08-24 18:48:12 +0530

    Time: 24 ms (45.30%), Space: 46 MB (84.59%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+        if(n==0)
+            return false;
+        long x =1;
+        
+        while(x<n)
+            x=x*3;
+        return (x==n);
+    }
+}
