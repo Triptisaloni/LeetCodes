9f47b8157b241a5bb8b433a0d289d4da8b82b7bc
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-09-12 23:54:06 +0530

    Time: 1 ms (92.22%), Space: 43.4 MB (78.16%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,9 +1,16 @@
 class Solution {
-    public void nextPermutation(int[] A) {
-        if(A == null || A.length <= 1) return;
+    public void nextPermutation(int[] A)
+    {
+        if(A == null || A.length <= 1) 
+            return;
+        
         int i = A.length - 2;
-        while(i >= 0 && A[i] >= A[i + 1]) i--; 
-        if(i >= 0) {                           
+        
+        while(i >= 0 && A[i] >= A[i + 1]) 
+            i--; 
+        
+        if(i >= 0) 
+        {                           
             int j = A.length - 1;              
             while(A[j] <= A[i]) j--;      
             swap(A, i, j);                     
@@ -11,13 +18,18 @@ class Solution {
         reverse(A, i + 1, A.length - 1);      
 }
 
-public void swap(int[] A, int i, int j) {
+public void swap(int[] A, int i, int j) 
+{
     int tmp = A[i];
     A[i] = A[j];
     A[j] = tmp;
 }
 
 public void reverse(int[] A, int i, int j) {
-    while(i < j) swap(A, i++, j--);
+    while(i < j) 
+        {     
+        swap(A, i, j);
+        i++; j--;
+        }
 }
 }
