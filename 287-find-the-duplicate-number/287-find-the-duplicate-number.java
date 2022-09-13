2af5a40930dc6f7ed79906f768efb2417c25148e
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-09-14 01:55:53 +0530

    Time: 10 ms (48.59%), Space: 76.1 MB (35.04%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,19 +1,27 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         
-	
-	int slow = nums[0];
-	int fast = nums[0];
-	do {
-		slow = nums[slow]; // one step pace
-		fast = nums[nums[fast]]; // fast = fast.next.next i.e. 2 steps pace
-	} while (slow != fast);
-
-	slow = nums[0];
-	while (slow != fast) {
-		slow = nums[slow]; // one step pace
-		fast = nums[fast]; // one step pace
-	}
-	return slow; // or fast
+        int n= nums.length;
+        if(n==1)
+            return -1;
+        int slow = nums[0];
+     int fast = nums[0];
+        
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while(slow!=fast);
+            
+            slow = nums[0];
+        
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
     }
 }
