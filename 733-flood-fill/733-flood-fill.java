bf9f78b79a0cf5be0fe50d1924c0c580ea68cd6e
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-10-06 19:59:22 +0530

    Time: 2 ms (33.75%), Space: 47.6 MB (78.79%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,21 +1,48 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-   //no need to do anything if oldColor and newColor are same
-        if(image[sr][sc]!=color) {
-            floodFill(image,sr,sc,color,image[sr][sc]);            
-        }
-        return image;
+        
+        int original = image[sr][sc];
+        if(color == original)
+            return image;
+        
+        // boolean vis[]
+        
+        dfs(image, sr, sc, color, original);
+            return image;
+            
     }
     
-    
-    private void floodFill(int[][] image, int i, int j, int newColor,int oldColor) {
-        if(i<0||j<0||i>=image.length||j>=image[0].length||image[i][j]!=oldColor||image[i][j]==newColor) {
+    public void dfs(int[][] image, int x, int y, int color, int original)
+    {
+        int n  = image.length;
+        int m = image[0].length;
+        
+        if(x<0 || y<0 || x>=n || y>=m || image[x][y] == color|| image[x][y]!=original)
             return;
+        
+        
+        image[x][y] = color;
+        
+        int [] r = new int[]{0,1,0,-1};
+        int [] c  = new int[]{1,0,-1,0};
+        
+        for(int i=0; i<4; i++)
+       { 
+            int p = x+r[i];
+            int q = y+c[i];
+            
+            if(p<n && p>=0 && q >=0 && q<m)
+            {
+                if(image[p][q]==original )
+                     dfs(image, p,q ,color,original);
+            }
+        
+        
         }
-        image[i][j]=newColor;
-        floodFill(image,i+1,j,newColor,oldColor);
-        floodFill(image,i,j+1,newColor,oldColor);
-        floodFill(image,i-1,j,newColor,oldColor);
-        floodFill(image,i,j-1,newColor,oldColor);
-}
+        
+        
+        // dfs(image, x , y+1 ,color,original);
+        // dfs(image, x-1 , y ,color,original);
+        // dfs(image, x , y-1 ,color,original);
+    }
 }
