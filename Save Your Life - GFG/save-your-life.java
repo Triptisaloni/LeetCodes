348831363ca450d869709900cead023c83743162
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-10-12 23:53:19 +0530

    Added solution - LeetHub

---
+++ Save Your Life - GFG/save-your-life.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+// } Driver Code Ends
+
+// atic String maxSum(String w,char x[],int b[], int n){
+//         //code here
+// }
+
+class Solution{
+
+    static String maxSum(String w,char x[],int b[], int n){
+
+        String str = "", strTemp = "";
+
+        int curSum = 0, maxSum = Integer.MIN_VALUE;
+
+        for(int i=0;i<w.length();i++){
+
+            char ch = w.charAt(i);
+
+            int j=0;
+
+            for(char c : x){
+
+                if(c==ch){
+
+                    break;
+
+                }
+
+                j++;
+
+            }
+
+            if(j==x.length){
+
+                curSum = curSum + (int)ch;
+
+                //str = str + ch;
+
+            }
+
+            else{
+
+                curSum = curSum + b[j];
+
+            }
+
+            strTemp = strTemp + ch;
+
+            if(curSum>maxSum){
+
+                maxSum = curSum;
+
+                str = strTemp;
+
+            }
+
+            if(curSum<0){
+
+                strTemp = "";
+
+                curSum=0;
+
+            }   
+
+        }
+
+        return str;
+
+    }
+
+}
+
+
+//{ Driver Code Starts.
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String w = read.readLine();
+            int n = Integer.parseInt(read.readLine());
+            String TE[] = read.readLine().trim().split(" ");
+            char x[] = new char[n];
+            for(int i = 0;i<n;i++)
+            {
+                x[i] = TE[i].charAt(0);
+            }
+            
+            String TR[] = read.readLine().trim().split(" ");
+            int b[] = new int[n];
+            for(int i = 0;i<n;i++)
+            {
+                b[i] = Integer.parseInt(TR[i]);
+            }
+           
+            Solution ob = new Solution();
+            System.out.println(ob.maxSum(w,x,b,n));
+        }
+    }
+}
+// } Driver Code Ends
