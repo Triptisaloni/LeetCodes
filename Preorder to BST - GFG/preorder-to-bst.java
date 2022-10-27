cc4d3bc69b0d7b67cc6fd593487fd16060902144
Tripti Saloni <54800383+Triptisaloni@users.noreply.github.com>
2022-10-28 01:13:12 +0530

    Added solution - LeetHub

---
+++ Preorder to BST - GFG/preorder-to-bst.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+class Node { 
+	int data; 
+	Node left, right; 
+	Node(int d) { 
+		data = d; 
+		left = right = null; 
+	} 
+} 
+
+
+class GFG {
+	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String[] inputline = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputline[0]);
+            inputline = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputline[i]);
+            }
+            
+            Node res = post_order(arr, n);
+            printPostorder(res);
+            System.out.println();
+        }
+    }
+    
+    
+    
+    
+// } Driver Code Ends
+//User function Template for Java
+
+
+public static Node post_order(int pre[], int size) 
+{
+ if(size==1) return new Node(pre[0]);
+  return make(pre,0,size-1);
+} 
+
+static Node make (int[]arr, int l, int r){
+    if(r<l)
+    return null;
+    
+    Node root = new Node (arr[l]);
+    
+    int ind=l ;
+    
+    for(int i =l+1; i<=r; i++)
+{
+        if(arr[i]>arr[l])
+        { ind = i-1; break;}
+        
+    }
+   root.left =  make(arr,l+1,ind);
+   root.right = make(arr,ind+1,r);
+   return root;
+}
+
+//{ Driver Code Starts.
+
+
+public static 	void printInorder(Node node) { 
+		if (node == null) { 
+			return; 
+		} 
+		printInorder(node.left); 
+		System.out.print(node.data + " "); 
+		printInorder(node.right); 
+	} 
+	
+public static 	void printPostorder(Node node) { 
+		if (node == null) { 
+			return; 
+		} 
+		printPostorder(node.left); 
+		printPostorder(node.right);
+		System.out.print(node.data + " "); 
+	} 
+	
+public static 	void printPreorder(Node node) { 
+		if (node == null) { 
+			return; 
+		} 
+		System.out.print(node.data + " "); 
+		printPreorder(node.left); 
+		printPreorder(node.right);
+	} 
+
+}
+// } Driver Code Ends
